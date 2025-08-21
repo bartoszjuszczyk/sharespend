fbf953858bad1281f838a300a408421a34cbbec8
Bartosz Juszczyk <b.juszczyk@bjuszczyk.pl>
2025-08-22 00:40:42 +0200

    initial commit

---
+++ app/SuccessLoginListener.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * File: SuccessLoginListener.php
+ *
+ * @author Bartosz Juszczyk <bartosz.juszczyk@ageno.pl>
+ * @copyright Copyright (C) 2025 Ageno (https://ageno.pl)
+ */
+
+namespace App;
+
+class SuccessLoginListener
+{
+    public function __construct()
+    {
+    }
+
+    public function handle($event): void
+    {
+        
+    }
+}
---
+++ database/migrations/2025_08_21_223120_update_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar')->nullable();
+            $table->string('currency')->default('USD');
+            $table->json('notif_prefs')->nullable();
+            $table->timestamp('last_login_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('avatar');
+            $table->dropColumn('currency');
+            $table->dropColumn('notif_prefs');
+            $table->dropColumn('last_login_at');
+        });
+    }
+};
